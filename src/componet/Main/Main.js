355b465c12bd1acaabdffb63f8c2d99db3902946
amibiplob <muhammadbiplob7@gmail.com>
2022-09-28 21:57:23 +0600

    css style added

--- src/componet/Main/Main.js
+++ src/componet/Main/Main.js
@@ -3,7 +3,7 @@ import Card from '../Card/Card';
 import "./Main.css"
 const Main = () => {
     return (
-        <div>
+        <div className='main'>
             <h1 className='title'>BIPLOB-CLUB</h1>
             <p>Select Your Item</p>
             <Card></Card>
--- src/componet/Profile/Profile.js
+++ src/componet/Profile/Profile.js
@@ -3,7 +3,32 @@ import './Profile.css'
 const Profile = () => {
     return (
         <div className='profile'>
-            <h1>profile</h1>
+            <div className='name'>
+                <img src="https://amibiplob.github.io/img/mypic%20(3).jpg" alt="" />
+                <h1>Biplob</h1>
+            </div>
+            <h1>Add A Break</h1>
+            <div className='break'>
+                <p>10</p>
+                <p>20</p>
+                <p>30</p>
+                <p>40</p>
+                <p>50</p>
+            </div>
+            <h1>Exercise Details</h1>
+            <div className='details'>
+                <h1>
+                Exercise time
+                </h1>
+                <h1>2000sec</h1>
+            </div>
+            <div className='details'>
+                <h1>
+                Break time
+                </h1>
+                <h1>2000sec</h1>
+            </div>
+            <button className='btn-complet'>Activity Completed</button>
         </div>
     );
 };
